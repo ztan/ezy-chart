c6ea960daaa6427b6613b573233746340202e21e
Zhendong Tan <ztan@bravurasolutions.com>
2017-11-27 16:18:12 +1030

    docs: add property descriptions

--- src/chart.component.ts
+++ src/chart.component.ts
@@ -145,56 +145,114 @@ export class ChartComponent implements OnDestroy, DoCheck {
 			});
 	}
 
+	/**
+	 * An array of strings, corresponding to Chart.ChartConfiguration.data.labels
+	 * @property
+	 */
 	@Input()
 	set labels(l: string[]) {
 		this._params.labels = l;
 	}
 
+	/**
+	 * The chart type, corresponding to Chart.ChartConfiguration.type
+	 * @default 'bar'
+	 * @property
+	 */
 	@Input()
 	set type(t: string) {
 		this._params.type = t;
 	}
 
+	/**
+	 * An array of Chart.ChartDataSets, corresponding to Chart.ChartConfiguration.data.datasets
+	 * @property
+	 */
 	@Input()
 	set datasets(ds: Chart.ChartDataSets[]) {
 		this._params.datasets = ds;
 	}
 
+	/**
+	 * An array of strings in hex, rgb, or rgba format, to define the base colors of datasets, or the data points of each dataset. If this
+	 * property is absent, or the colors defined are less than the number of datasets (or the data points of each dataset), the missing colors will
+	 * be assigned from the defult palette
+	 * @property
+	 */
 	@Input()
 	set colors(c: string[]) {
 		this._params.colors = c;
 	}
 
+	/**
+	 * Specify what the colors (either defined or generated) are for.
+	 *  * 'series' - each color corresponds to a dataset
+	 *  * 'data' - each color corresponds to a data point of each dataset
+	 *  * 'auto' - use 'series' for chart types 'bar', 'horizontalBar' and 'line'; and use 'data' for other chart types
+	 *  * 'none' - turn off the color generator. Use this value if colors are specified via #options
+	 * @default 'auto'
+	 * @property
+	 */
 	@Input()
 	set colorsFor(cf: ColorsForType) {
 		this._params.colorsFor = cf;
 	}
 
+	/**
+	 * The aspect ratio of the chart
+	 * @default 2
+	 * @property
+	 */
 	@Input()
 	set ratio(r: number) {
 		this._params.ratio = r;
 	}
 
+	/**
+	 * Specifiy how to display the legend
+	 *  * if the value is a boolean, it corresponds to Chart.ChartConfiguration.options.legend.display
+	 *  * if the value is 'auto', the legend will be arranged automatically. For example, the legend will be hidden if the chart area is too small
+	 *  * if the value is of type Chart.ChartLegendOptions, it corresponds to Chart.ChartConfiguration.options.legend.position
+	 *  * if the value is of type Chart.ChartLegendOptions, it corresponds to Chart.ChartConfiguration.options.legend
+	 * @default 'auto'
+	 * @property
+	 */
 	@Input()
 	set legend(l: LegendType) {
 		this._params.legend = l;
 	}
 
+	/**
+	 * An ISO 4217 currency code. If specified, it will be used to format the scales of the main axis, as well as the values in the tooltips.
+	 * @property
+	 */
 	@Input()
 	set currency(curr: string) {
 		this._params.currency = curr;
 	}
 
+	/**
+	 * Corresponds to Chart.ChartConfiguration.options. This overrides any other settings.
+	 * @property
+	 */
 	@Input()
 	set options(opt: Chart.ChartOptions) {
 		this._params.options = opt;
 	}
 
+	/**
+	 * A moment.js format string. When specified, the X axis will be configured with time scales, and this value will be used to format the tooltips.
+	 * @property
+	 */
 	@Input()
 	set timeFormat(tf: string) {
 		this._params.timeFormat = tf;
 	}
 
+	/**
+	 * Specify whether to show the overall percentage values in the popover tooltips.
+	 * @property
+	 */
 	@Input()
 	set percentage(p: ShowPercentageType) {
 		this._params.percentage = p;
