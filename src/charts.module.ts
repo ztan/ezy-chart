13a2fc5e9e066635ad3b09659d18c71fd3d35c8f
Zhendong Tan <ztan@bravurasolutions.com>
2017-12-05 11:51:48 +1030

    feat: change cursor type on hovering legend boxes

--- src/charts.module.ts
+++ src/charts.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartComponent } from './chart.component';
+import * as Chart from 'chart.js';
 
 @NgModule({
 	declarations: [ChartComponent],
@@ -10,3 +11,24 @@ import { ChartComponent } from './chart.component';
 export class ChartsModule {}
 
 export { ChartComponent };
+
+Chart.pluginService.register({
+	afterEvent: (chartInstance: Chart, chartEvent: MouseEvent) => {
+		const legend = (chartInstance as any).legend;
+		const canvas = chartInstance.canvas;
+		const x = chartEvent.x;
+		const y = chartEvent.y;
+		let cursorStyle = 'default';
+		if (x <= legend.right && x >= legend.left && y <= legend.bottom && y >= legend.top) {
+			for (const box of legend.legendHitBoxes) {
+				if (x <= box.left + box.width && x >= box.left && y <= box.top + box.height && y >= box.top) {
+					cursorStyle = 'pointer';
+					break;
+				}
+			}
+		}
+		if (canvas) {
+			canvas.style.cursor = cursorStyle;
+		}
+	}
+});
